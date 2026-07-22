d87522e71249714aa0658199f3424d08b387368b
DanielC <danielcamachogomez392@gmail.com>
2026-07-21 20:06:23 -0600

    Corrección de ejercicios

--- Python intermedio/Big O Notation/Ejercicio1_Big_O.py
+++ Python intermedio/Big O Notation/Ejercicio1_Big_O.py
@@ -1,4 +1,4 @@
-def bubble_sort(self): 
+def bubble_sort(self): # O(n^2)
         if self.head is None or self.head.next is None: # O(1)
             return # O(1)
         
@@ -10,7 +10,7 @@ def bubble_sort(self):
 
             current = self.head # O(1)  
 
-            while current.next is not None: # O(n)
+            while current.next is not None: # O(n^2)
 
                 if current.data > current.next.data: # O(1) 
                     current.data, current.next.data = current.next.data, current.data # O(1)
@@ -18,3 +18,5 @@ def bubble_sort(self):
                     swapped = True # O(1)
 
                 current = current.next # O(1)
+
+# El algoritmo analizado es O(n^2)
